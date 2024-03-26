d8f8484d375e351cf93817878ec649499d2d515b
emreyurur <emreyrr81@gmail.com>
2024-03-27 01:36:40 +0300

    LoginScreen'de düzeltme

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,96 +1,216 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, Image, Linking, TextInput } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
+import nacl from 'tweetnacl';
+import base58 from 'bs58';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { encode } from 'bs58';
+import axios from 'axios';
 
-interface ButtonProps {
-  onPress: (voteNumber: number) => void;
-  voteNumber: number;
-  text: string;
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+
+interface KeyPair {
+  publicKey: Uint8Array;
+  secretKey: Uint8Array;
 }
 
-interface OrganizationScreenProps {
-  userPublicKey: Uint8Array;
-}
-
-const CustomButton: React.FC<ButtonProps> = ({ onPress, voteNumber, text }) => {
-  const navigation = useNavigation();
-
-  const handlePress = () => {
-    onPress(voteNumber);
-    navigation.navigate('VotingDetailScreen');
-  };
-
-  // Tüm buttonların rengini ve boyutunu 2. buttonun renkleri ve boyutu olarak ayarlayın
-  const colors = ['#FF6EDF', '#7B61FF'];
-
-  return (
-    <TouchableOpacity onPress={handlePress} style={styles.button}>
-      <LinearGradient colors={colors} style={styles.gradient} start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }}>
-        <Text style={styles.buttonText}>{text}</Text>
-      </LinearGradient>
-    </TouchableOpacity>
-  );
+type RootStackParamList = {
+  LoginScreen: undefined; // Assuming LoginScreen doesn't expect any parameters
+  OrganizationScreen: { userPublicKey: Uint8Array }; // Define parameters expected by OrganizationScreen
+  // Define other screens and their parameters here
 };
 
-const OrganizationScreen: React.FC<OrganizationScreenProps> = ({ userPublicKey }) => {
-  const handleButtonPress = (voteNumber: number) => {
-    console.log('Pressed Vote', voteNumber);
+
+const LoginScreen: React.FC = () => {
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [dappKeyPair, setDappKeyPair] = useState<KeyPair | null>(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const generateRandomKeyPair = () => {
+      try {
+        const newKeyPair: KeyPair = nacl.box.keyPair();
+        setDappKeyPair(newKeyPair);
+      } catch (error) {
+        console.error('Error generating random key pair:', error);
+      }
+    };
+
+    generateRandomKeyPair();
+  }, []);
+
+  const handleConnectPhantom = async () => {
+    if (dappKeyPair) {
+      const params = new URLSearchParams({
+        dapp_encryption_public_key: base58.encode(dappKeyPair.publicKey),
+        cluster: 'mainnet-beta',
+        app_url: 'https://phantom.app',
+        redirect_link: 'myapp://onConnect',
+      });
+  
+      const connectUrl = `phantom://v1/connect?${params.toString()}`;
+  
+      try {
+        await Linking.openURL(connectUrl);
+        setModalVisible(false); // Assuming this sets the visibility of a modal
+        await saveWalletPublicKey(dappKeyPair.publicKey);
+        // Ensure navigation.navigate is called with parameters that match the expected types
+        navigation.navigate("OrganizationScreen", { userPublicKey: dappKeyPair.publicKey });
+      } catch (error) {
+        console.error('Error connecting to Phantom:', error);
+      }
+    }
   };
 
+  
+const saveWalletPublicKey = async (publicKey: Uint8Array) => {
+  try {
+    // Public key'i base58 ile kodlayıp bir stringe dönüştür
+    const publicKeyString = encode(publicKey);
+
+    const response = await axios.get('http://192.168.107.244:3000/getProposal')
+      // headers: {
+      //   'Content-Type': 'application/json',
+      // },
+      // data: { public_key: publicKeyString }, // Eğer body parametresi gerekiyorsa kullanabilirsiniz
+    
+
+  console.log(response.data)
+  } catch (error) {
+    console.error('Error saving wallet public key:', error);
+  }
+};
+
+
   return (
-    <LinearGradient colors={['#ACE2E1', '#F7EEDD']} style={styles.fullScreen}>
+    <LinearGradient colors={['#ACE2E1', '#F7EEDD']} style={styles.linearGradient}>
       <View style={styles.container}>
-        <CustomButton
-          onPress={handleButtonPress}
-          voteNumber={1}
-          text="Vote for Option 1"
-        />
-        <CustomButton
-          onPress={handleButtonPress}
-          voteNumber={2}
-          text="Vote for Option 2"
-        />
-        <CustomButton
-          onPress={handleButtonPress}
-          voteNumber={3}
-          text="Vote for Option 3"
-        />
-        {/* Additional buttons can be added here */}
+        <Image style={styles.daoLogo} source={require('../assets/dao_logo.png')} />
+        <TouchableOpacity style={styles.button} onPress={() => setModalVisible(true)}>
+          <Image style={styles.phantomIcon} source={require('../assets/phantom_icon.png')} />
+          <Text style={styles.buttonText}>Connect Phantom</Text>
+        </TouchableOpacity>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.modalBackground}>
+            <View style={styles.centeredView}>
+              <TouchableOpacity style={styles.exitButton} onPress={() => setModalVisible(false)}>
+                <Image source={require('../assets/remove-button.png')} style={styles.exitIcon} />
+              </TouchableOpacity>
+              <View style={styles.modalView}>
+                <Image style={styles.phantomIcon} source={require('../assets/phantom_icon.png')} />
+                <Text style={styles.modalHeaderText}>Choose an Option</Text>
+                <TouchableOpacity
+                  style={styles.modalOption}
+                  onPress={() => {
+                    Linking.openURL('https://phantom.app/');
+                  }}>
+                  <Text style={styles.modalOptionText}>Create a New Wallet</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.modalOption} onPress={handleConnectPhantom}>
+                  <Text style={styles.modalOptionText}>I Have a Already Wallet</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </Modal>
       </View>
     </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
-  fullScreen: {
-    flex: 1, // Ekranın tamamını kaplamasını sağlar
+  linearGradient: {
+    flex: 1,
+  },
+  daoLogo: {
+    height: 500,
+    width: 500,
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
-    width: 300, // Genişlik 300 olarak ayarlandı
-    height: 80, // Yükseklik 80 olarak ayarlandı
-    justifyContent: 'center',
+    margin: 20,
+    backgroundColor: '#36008D',
+    padding: 25,
+    borderRadius: 40,
     alignItems: 'center',
-    marginVertical: 15,
-    borderRadius: 40, // Border yarı çapı 40 olarak ayarlandı
-    overflow: 'hidden',
+    flexDirection: 'row',
+    
   },
-  gradient: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+  phantomIcon: {
+    height: 40,
+    width: 40,
+    marginRight: 10,
   },
   buttonText: {
-    fontSize: 24, // Yazı boyutu 24 olarak ayarlandı
-    color: 'white',
+    fontSize: 25,
     fontWeight: 'bold',
+    color: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    textAlign: 'center',
   },
-});
-
-export default OrganizationScreen;
+  modalBackground: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust opacity for the desired blurring effect
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centeredView: {
+    alignItems: 'center',
+  },
+  exitButton: {
+    position: 'absolute',
+    top: 25,
+    left: 25,
+    zIndex: 1,
+  },
+  exitIcon: {
+    height: 30,
+    width: 30,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: '#27005D',
+    borderRadius: 20,
+    padding: 30,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalHeaderText: {
+    fontSize: 24,
+    color: '#fff',
+    marginBottom: 20,
+  },
+  modalOption: {
+    backgroundColor: '#4D0099',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    marginBottom: 15,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center', // Center the content horizontally
+    fontSize: 25, // Aynı font boyutu
+    },
+    modalOptionText: {
+    fontSize: 20,
+    color: '#fff',
+    },
+    });
+    
+    export default LoginScreen;
